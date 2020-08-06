4286f952637bd672399d9137d4932a32c6e11610
asferdem <ferdem4254@gmail.com>
2020-08-05 21:22:18 -0400

    add MissJava.java

--- gitDemo1/src/Test.java
+++ gitDemo1/src/Test.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("git fetch");
+        System.out.println("git merge");
+    }
 }
