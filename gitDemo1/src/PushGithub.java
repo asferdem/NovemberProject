67eea65bdf45f2cc0eea8418e46883f68b5f2e58
asferdem <ferdem4254@gmail.com>
2020-08-05 21:26:41 -0400

    add MissJava.java

---
+++ gitDemo1/src/PushGithub.java
@@ -0,0 +1,10 @@
+public class PushGithub {
+    public static void main(String[] args) {
+         //i write github
+    System.out.println("test to puul git hub");
+
+        // deneme
+//**************************************************
+    }
+}
+
