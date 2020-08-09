2741b247150458b16024d26136f88dc5bfa481d4
asferdem <ferdem4254@gmail.com>
2020-08-09 12:16:31 -0400

    Update1

--- Summer2020_B20/src/day39_CustomClass/CarObjects.java
+++ Summer2020_B20/src/day39_CustomClass/CarObjects.java
@@ -4,13 +4,17 @@ public class CarObjects {
 
     public static void main(String[] args) {
        Car car1=new Car();
-
+/*
        car1.brand = "Toyota";
        car1.model="Sienna";
        car1.color="White";
        car1.year=2020;
        car1.mileage=1356.5;
        car1.price=35000;
+
+ */
+       car1.setInfo("Toyota","Sienna",2020,"White",1356.5,35000);
+
 
         System.out.println(car1.brand);
         System.out.println(car1.model);
@@ -20,7 +24,7 @@ public class CarObjects {
         System.out.println("================================================");
 
         Car car2= new Car();
-
+/*
         car2.brand = "BMW";
         car2.model="X5";
         car2.color="Black";
@@ -28,13 +32,14 @@ public class CarObjects {
         car2.mileage=500.2;
         car2.price=30000.21;
 
-
-
-        System.out.println(car2.brand);
+ */
+        car2.setInfo("BMW","X 5",2019,"Red",1000.5,30000);
         System.out.println(car2.brand);
         System.out.println(car2.model);
         System.out.println(car2.year);
         System.out.println(car2.color);
+        System.out.println(car2.mileage);
+        System.out.println(car2.price);
     }
 
 
