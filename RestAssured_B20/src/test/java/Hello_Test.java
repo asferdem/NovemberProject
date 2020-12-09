5f22a918dc061cc119b5f19ff60bcaca4cd3aa21
asferdem <ferdem4254@gmail.com>
2020-12-08 19:49:45 -0500

    Before After method

--- RestAssured_B20/src/test/java/Hello_Test.java
+++ RestAssured_B20/src/test/java/Hello_Test.java
@@ -1,11 +1,48 @@
 import org.junit.jupiter.api.*;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Hello_Test {
+    @BeforeAll
+    public static void setUp(){
 
+        System.out.println("@BeforeAll is running ");
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        System.out.println("@AfterAll is Running");
+    }
+
+    @BeforeEach
+    public void setupTest(){
+        System.out.println("Before each running ");
+    }
+
+    @AfterEach
+    public void tearDownTest(){
+        System.out.println("After each running ");
+    }
 
     @Test
     public  void test(){
+        System.out.println("Test is running ");
         Assertions.assertEquals(4,3+1);
     }
+
+    @Test
+    public  void test2(){
+        System.out.println("Test2 is running ");
+        assertEquals(12,4*3);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
