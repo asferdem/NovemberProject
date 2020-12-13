94b6ff1d2c544f29a537aaf27d9f1efc691995da
asferdem <ferdem4254@gmail.com>
2020-12-13 12:51:04 -0500

    update

--- RestAssured_B20/src/test/java/Day04/LibraryAppTest.java
+++ RestAssured_B20/src/test/java/Day04/LibraryAppTest.java
@@ -3,15 +3,14 @@ import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
-
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
 public class LibraryAppTest {
 //Librarian1  email 	librarian69@library
 //Librarian1  password		KNPXrm3S
+static  String myToken="";
         @BeforeAll
         public static void setUp(){
             baseURI = "http://library1.cybertekschool.com";
@@ -25,6 +24,7 @@ public class LibraryAppTest {
     @DisplayName("Testing POST /login Endpoint")
     @Test
     public void testLogin(){
+         myToken=
             given()
                     .log().all()
                     .contentType(ContentType.URLENC)
@@ -38,12 +38,31 @@ public class LibraryAppTest {
                     .statusCode(200)
                     .contentType(ContentType.JSON)
                     .body("token",is(not(emptyString())))
+                    .extract()
+                    .jsonPath()
+                    .getString("token")
                     ;
+        System.out.println("myToken = " + myToken);
 
 
     }
 
+    @DisplayName("Testing GET /dashboard_stats Endpoint")
+    @Test
+    public void ZtestDashboard_stats(){
 
+            given()
+                    .log().all()
+                    .header("x-library-token",myToken).
+            when()
+                    .get("/dashboard_stats").
+                    then()
+                    .log().all()
+                    .assertThat()
+                    .statusCode( is(200) )
+                    .contentType(ContentType.JSON)
+            ;
+    }
 
 
 
